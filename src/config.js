f9e32ec92a1cc328b1a6e41fd309fb5e22ec93e4
asherhoffman1120 <asher.hoffman1120@gmail.com>
2022-07-25 13:38:39 +0900

    Final version

---
+++ src/config.js
@@ -0,0 +1,21 @@
+export const PROGRAM_ID = "Zq5BGzpnYDgcYTB7v5mgaSg4buQbXvEu9JE8gFe9NPJ";
+
+export const PUBLISH_NETWIRK = "devnet"; // "devnet" or "mainnet"
+
+export const ADMIN_KEY = "Ek464DsENbDmMWTxSBNRn1S3xuxkixwiS5ceWoRLSjFw";
+
+export const INIT_WALLETKEY = "Ek464DsENbDmMWTxSBNRn1S3xuxkixwiS5ceWoRLSjFw";
+
+export const LOCK_TIME = 1000 * 60 * 2 // 10 is days
+
+export const REWARD_EPOCH = 1000 * 60 * 3 // Daily
+
+export const REWARD_UNIT = 0.02 // SOL
+
+export const LAND_CREATOR = "69JUqMCBEyKBppr4zwGAM9HTQ8JtUwWE2vdQQShWQExH";
+export const FARMER_CREATOR = "9nwyX2j8o52TQChq7whMRVH3p5DDyv7wWefjrXeYASnC";
+export const ANIMAL_CREATOR = "DrbC3kbP57EA1wQqjCdaEMKBUEjESGTZbNY7vrZxP5Tb";
+
+export const LAND_MAX = 1;
+export const FARMER_MAX = 3;
+export const ANIMAL_MAX = 3;
---
+++ src/pages/StakedYards.js
@@ -0,0 +1,36 @@
+import YardItem from "../components/YardItem";
+import {useNavigate} from "react-router-dom";
+
+export default function StakedYards({
+    yards,
+    onWithdrawFromYard,
+    unstakeLoading,
+    ...props
+}) {
+    const navigate = useNavigate()
+    return (
+        <div className="main-content">
+            {
+            yards.yards.length !== 0 ? yards.yards.map((item, key) => (
+                <YardItem key={key}
+                    yard={item}
+                    onWithdrawFromYard={onWithdrawFromYard}
+                    unstakeLoading={unstakeLoading}
+                    number={
+                        key + 1
+                    }
+                    stakedTime={
+                        item.stakedTime
+                    }/>
+            )) : <div className="empty-yards">
+                    <p>There is no Staked Yard.</p>
+                    <button className="to-nfts"
+                        onClick={
+                            () => navigate("/")
+                    }>
+                        Go to Stake
+                    </button>
+                </div>
+        } </div>
+    );
+}
